d6a7774dd0bd68ffb9d7b11fe8155dc027c2fe18
MrFlynn <nick@pleatsikas.me>
2017-05-29 23:48:25 -0700

    Added compile_files script.

---
+++ compile_files/README.md
@@ -0,0 +1,8 @@
+## compile_files
+
+Moves files of specific extension in folder subtree to new folder tree. New 
+folder tree is based on the name of the file.
+
+Make sure you have Python 3.5+ installed in order to run.
+
+Running the command is as simple as `python3 compile_files.py`.
---
+++ compile_files/compile_files.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+"""
+Imports:
+"""
+import shutil
+import glob
+import os
+
+"""
+Global vars:
+"""
+extension = '.asm'  # Modify this.
+directive = './**/*{0}'
+
+files_dirs = glob.glob(directive.format(extension, ), recursive=True)
+
+"""
+Functions:
+"""
+
+# createDirName : str -> str
+# Gets the directory name for a passed in filename.
+def createDirName(filename: str) -> str:
+    return (filename.split('/')[-1]).split("_")[0]
+
+# copyFile : str ->
+# Copies the given file to a new directory.
+def copyFile(file: str):
+    dirname = createDirName(file)
+
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    currdir = '{0}/{1}'.format(os.getcwd(), file[1:], )
+    movedir = '{0}/{1}'.format(os.getcwd(), dirname, )
+    shutil.copy2(currdir, movedir)
+
+# main:
+def main():
+    for file in files_dirs:
+        copyFile(file)
+
+# Runner:
+if __name__ == '__main__':
+    main()
