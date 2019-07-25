4570b366962c9b3fbd399ecb77301e4b255a98b6
MrFlynn <nick@pleatsikas.me>
2019-07-24 23:04:12 -0700

    Updated application entrypoint with sample help.
    This is just so I could test something without doing a full debug
    of the actual application this roughly models.

--- py_async_experiment/__main__.py
+++ py_async_experiment/__main__.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python
+
+"""This is an example application meant to test the fundamental design of an
+application I am writing for my job.
+
+It served as a testbed for what design would best accomplish the overall goal
+of this application.
+"""
+
 import argparse
+import sys
 
 from app import App
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=sys.modules[__name__].__doc__
+    )
     parser.add_argument('--thread-timings', dest='timings', type=int, nargs='+')
 
     args = parser.parse_args()
